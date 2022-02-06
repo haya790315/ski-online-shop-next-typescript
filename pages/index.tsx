bae2b747b1a8a08e5996cd102f870be25bb641ee
haya790315 <haya790315@gmail.com>
2022-02-06 12:42:53 +0900

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return <div className="text-2xl text-green-300 sm:underline font-serif md:text-blue-400 no-underline">
+    hello world
+  
+  </div>;
+};
+
+export default Home;
