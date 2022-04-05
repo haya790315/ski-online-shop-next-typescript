be623b3eec1d66d8439800678b3320cffbc3130b
haya790315 <haya790315@gmail.com>
2022-04-05 11:03:47 +0900

    update font-family

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -33,8 +33,6 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>SKI JAPAN</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link rel="icon" href="/image/BrandLogo/favicon.ico" />
       </Head>
       {isLoading && <LoadingSpin />}
 
---
+++ pages/_document.js
@@ -0,0 +1,22 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href="/image/BrandLogo/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Kosugi+Maru&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
