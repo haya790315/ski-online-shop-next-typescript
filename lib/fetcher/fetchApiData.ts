fdde061179482c3cf5ed8b2038706124936cbd60
haya790315 <haya790315@gmail.com>
2022-04-18 15:20:54 +0900

    update env.local

--- lib/fetcher/fetchApiData.ts
+++ lib/fetcher/fetchApiData.ts
@@ -1,17 +1,11 @@
-export const fetchApiData = async (api:string) => {
+export const fetchApiData = async (api: string) => {
   const res = await fetch(api);
   const response = await res.json();
-  const data = response.result
+  const data = response.result;
 
   if (res.status !== 200) {
-<<<<<<< HEAD
-    throw new Error(response.message);
-=======
-    console.error(data.message)
->>>>>>> dev
+    console.error(data.message);
   }
-  
+
   return data;
 };
-
-
